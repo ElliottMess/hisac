760623da85b919bb5047fc8afb5d6478dd061cac
ElliottMess <36757002+ElliottMess@users.noreply.github.com>
2024-01-28 11:03:30 +0000

    Update BASE_DIR in settings.py

--- hisac/settings.py
+++ hisac/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 from django.conf import settings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
